d6b8ab50c5b78802f62da29bd166fbd70e83a5a3
Hector Urtubia <urtubia@gmail.com>
2026-02-03 21:08:00 -0800

    manual update

--- src/ui/miniacid_display.cpp
+++ src/ui/miniacid_display.cpp
@@ -287,7 +287,7 @@ void MiniAcidDisplay::drawSplashScreen() {
   centerText(info_y + small_h, "Space - to start/stop sound", COLOR_WHITE);
   centerText(info_y + 2 * small_h, "ESC - for help on each page", COLOR_WHITE);
   
-  centerText(info_y + 3 * small_h + 5, "v0.0.8-dev", IGfxColor::Gray());
+  centerText(info_y + 3 * small_h + 5, "v0.0.8", IGfxColor::Gray());
   
   // char build_info[64];
   // snprintf(build_info, sizeof(build_info), "Built: %s %s", __DATE__, __TIME__);
